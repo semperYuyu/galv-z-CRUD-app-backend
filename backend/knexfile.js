624d27be39fdb85eaea01b4d598f5e4d6ee4c21d
Tafari <tafarigriffith2016@gmail.com>
2026-06-29 10:30:36 -0600

    Changes knexfile.js to take DATABASE_URL properly

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -6,11 +6,13 @@ module.exports = {
   development: {
     client: "pg",
     connection: {
-      host: process.env.DATABASE_URL ? "db" : "127.0.0.1",
-      port: 5432,
-      user: "postgres",
-      password: "postgres",
-      database: "inventory_management",
+      host: process.env.DATABASE_URL || {
+        host: "127.0.0.1",
+        port: 5432,
+        user: "postgres",
+        password: "postgres",
+        database: "inventory_management",
+      },
     },
     pool: { min: 2, max: 10 },
     acquireConnectionTimeout: 10000,
